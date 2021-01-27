a348fe53e449bc8dcf2dd2c8ae5ce42011dd1780
Jan <26145882+imverum@users.noreply.github.com>
2021-01-27 10:26:37 +0100

    fix crash when bot cant dm user

--- modules/bot/remindmeManager.js
+++ modules/bot/remindmeManager.js
@@ -46,27 +46,31 @@ module.exports = {
         timeouts = [];
         reminders.keyArray().forEach(u => {
             Object.keys(reminders.get(u)).forEach(o => {
-                const now = Date.now();
-                const details = reminders.get(u)[o];
-                const timer = details.time - now;
-                timeouts.push(setTimeout(function() {
-                    let mesg = new Discord.MessageEmbed()
-                    .setTitle('Here\'s your reminder!')
-                    .setDescription(
-                        `${details.msg ? `> ${
-                            details.msg.replace(new RegExp('\n', 'g'), '')}\n[Jump to message](${details.url})` : `You didn't set a message, click [here](${details.url}) to show context`
-                        }`
-                    )
-                    .setFooter(`You received this message because you used the ${getPrefix()}remindme command.`)
-                    .setTimestamp(details.createdAt)
-                    .setColor('4f545c')
-                    if (client.users.cache.get(u)) client.users.cache.get(u).send(mesg);
-                    let l = reminders.get(u);
-                    delete l[o];
-                    reminders.set(u, l);
-                    delete l;
-                }, timer));
-            })
+                try {
+                    const now = Date.now();
+                    const details = reminders.get(u)[o];
+                    const timer = details.time - now;
+                    timeouts.push(setTimeout(function() {
+                        let mesg = new Discord.MessageEmbed()
+                        .setTitle('Here\'s your reminder!')
+                        .setDescription(
+                            `${details.msg ? `> ${
+                                details.msg.replace(new RegExp('\n', 'g'), '')}\n[Jump to message](${details.url})` : `You didn't set a message, click [here](${details.url}) to show context`
+                            }`
+                        )
+                        .setFooter(`You received this message because you used the ${getPrefix()}remindme command.`)
+                        .setTimestamp(details.createdAt)
+                        .setColor('4f545c')
+                        if (client.users.cache.get(u)) client.users.cache.get(u).send(mesg).catch(console.warn);
+                        let l = reminders.get(u);
+                        delete l[o];
+                        reminders.set(u, l);
+                        delete l;
+                    }, timer));
+                } catch(e) {
+                    console.error(e);
+                }
+            });
         })
     }
 }
