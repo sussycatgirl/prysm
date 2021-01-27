515d2f30a3ea21c5b679cb94c6cfcce5ae0eaf92
Jan <26145882+imverum@users.noreply.github.com>
2021-01-27 14:35:11 +0100

    bruh

---
+++ modules/commands/slash/getthisoffofmyscreen.js
@@ -0,0 +1,12 @@
+const { SlashCommand } = require('../../bot/slashCommands');
+
+/**
+ * 
+ * @param {SlashCommand} cmd 
+ */
+module.exports.execute = (cmd, callback) => {
+    callback('\u200b\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+        + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\u200b', true);
+}
+
+module.exports.sendConfirmation = 'callback';
