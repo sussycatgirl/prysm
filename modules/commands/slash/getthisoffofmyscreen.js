51f1c48cd610c94c436a5f90dc8402187642061e
Jan <26145882+imverum@users.noreply.github.com>
2021-01-31 20:51:46 +0100

    fix it so it works with android app

--- modules/commands/slash/getthisoffofmyscreen.js
+++ modules/commands/slash/getthisoffofmyscreen.js
@@ -8,8 +8,10 @@ const { InteractionResponseType: resType } = require('discord-interactions');
  * @param {function(String, Discord.MessageEmbed | false, resType, boolean) : void} callback
  */
 module.exports.execute = (cmd, callback) => {
-    callback('\u200b\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
-        + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\u200b', false, resType.CHANNEL_MESSAGE, true);
+    const line = '\u200b\n';
+    let text = '';
+    for (let i=0; i<40; i++) text += line; // 40 lines is enough for Discord on normal zoom on a full HD screen
+    callback(text, false, resType.CHANNEL_MESSAGE, true);
 }
 
 module.exports.sendConfirmation = 'callback';
