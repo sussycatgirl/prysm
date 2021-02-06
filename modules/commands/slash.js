3cd0710c4470f0baf7302d469ded05d22de18aaf
Jan <26145882+imverum@users.noreply.github.com>
2021-02-06 23:06:00 +0100

    oop

--- modules/commands/slash.js
+++ modules/commands/slash.js
@@ -22,7 +22,7 @@ module.exports.execute = async (message, args) => {
     let embed = new Discord.MessageEmbed();
     embed.setTitle('Slash commands');
     embed.setDescription(`Slash commands are a new easy way to interact with bots. To enable them for this server, simply ${message.member.permissions.has('MANAGE_GUILD') ? `click [here](${url})` : `tell someone with \`Manage Server\` permission to [authorize](${url}) the bot`}.\n[Learn more](https://support.discord.com/hc/sv/articles/1500000368501-Slash-Commands-FAQ) about slash commands.`);
-    if (message.channel.type != 'dm' && message.channel.permissionsFor(message.guild?.me).has('ATTACH_FILES'))
+    if (message.channel.type == 'dm' || message.channel.permissionsFor(message.guild?.me).has('ATTACH_FILES'))
         embed.attachFiles([ new Discord.MessageAttachment(await fs.readFile('assets/slash.gif'), 'image.gif') ]);
     embed.setImage('attachment://image.gif');
     
